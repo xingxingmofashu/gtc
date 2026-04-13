caf800777510247de4a32082eeb4a8328f4d2c8e
xingxingmofash <xingxingmofashu@outlook.com>
2026-04-13 15:24:12 +0800

    feat: init cli

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+import yargs from "yargs"
+import { hideBin } from "yargs/helpers"
+import pkg from "../package.json"
+
+const cli = yargs(hideBin(process.argv))
+  .parserConfiguration({ "populate--": true })
+  .scriptName("gc")
+  .wrap(yargs().terminalWidth())
+  .help("help", "show help")
+  .alias("h", ["help"])
+  .version("version", pkg.version)
+  .alias("v", ["version"])
+  .strict()
+
+try {
+  await cli.parse()
+} catch (error) {
+  if (error instanceof Error) {
+    console.error(error.message)
+  }
+  process.exitCode = 1
+} finally {
+  process.exit()
+}
