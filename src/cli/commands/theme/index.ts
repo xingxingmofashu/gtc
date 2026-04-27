f0ccb188d047a04527468caf0bec79430378152b
xingxingmofash <xingxingmofashu@outlook.com>
2026-04-27 10:14:51 +0800

    feat: update theme command

--- src/cli/commands/theme/index.ts
+++ src/cli/commands/theme/index.ts
@@ -1,4 +1,5 @@
 import { cmd } from "../../utils/cmd"
+import { ThemeSetCommand } from "./set"
 import { ThemeInstallCommand } from "./install"
 import { ThemeListCommand } from "./list"
 import { ThemeRemoveCommand } from "./remove"
@@ -11,6 +12,7 @@ export const ThemeCommand = cmd({
       .command(ThemeListCommand)
       .command(ThemeInstallCommand)
       .command(ThemeRemoveCommand)
+      .command(ThemeSetCommand)
       .demandCommand(),
   handler: async () => {},
 })
